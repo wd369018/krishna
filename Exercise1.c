fe2fa9d24355f98c1c8844283e28e70da8c0ce38
wd369018 <155811536+wd369018@users.noreply.github.com>
2024-01-08 17:23:37 +0530

    Add files via upload

---
+++ Exercise1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/*
+print a multiplication table of a number by the user in pretty form
+
+Example:
+Input
+Enter the number you want multiplication table of:
+6
+
+Output:
+Table of 6:
+6x1=6
+6x2=12
+6x3=18
+6x4=24
+6x5=30
+6x6=36
+6x7=42
+6x8=48
+6x9=54
+6x10=60
+
+
+*/
+int main()
+{
+    /* code*/
+    return 0;
+}
